f175b51d9e54204f76abb4d6b80f486a6198eda0
mohamad407 <asihfibrahim600@gmail.com>
2026-09-22 17:25:10 +0530

    Import and invoke disableInspect utility

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,7 +4,10 @@ import { BrowserRouter } from 'react-router-dom';
 import { Toaster } from 'react-hot-toast';
 import App from './App.jsx';
 import { AuthProvider } from './context/AuthContext.jsx';
+import { disableInspect } from './utils/disableInspect.js';
 import './styles/index.css';
+
+disableInspect();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
